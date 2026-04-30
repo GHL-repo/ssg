25af9b73e8379ae629b2e84a27c90a1f4058652c
Guus <ghligtvoet@gmail.com>
2026-04-30 15:04:57 +0200

    add node splitting functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from src.textnode import TextNode, TextType
 
 
@@ -18,3 +20,83 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 else:
                     new_nodes.append(TextNode(phrase, text_type))
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        extracted_images = extract_markdown_images(old_node.text)
+
+        if not extracted_images:
+            new_nodes.append(old_node)
+            continue
+
+        current_text = old_node.text
+
+        for alt, url in extracted_images:
+            delimiter = f"![{alt}]({url})"
+            split_text = current_text.split(delimiter, 1)
+
+            if len(split_text) != 2:
+                raise ValueError("malformed markdown")
+
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+
+            current_text = split_text[1]
+
+        if current_text != "":
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        extracted_links = extract_markdown_links(old_node.text)
+
+        if not extracted_links:
+            new_nodes.append(old_node)
+            continue
+
+        current_text = old_node.text
+
+        for text, url in extracted_links:
+            delimiter = f"[{text}]({url})"
+            split_text = current_text.split(delimiter, 1)
+
+            if len(split_text) != 2:
+                raise ValueError("malformed markdown")
+
+            if split_text[0] != "":
+                new_nodes.append(TextNode(split_text[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(text, TextType.LINK, url))
+
+            current_text = split_text[1]
+
+        if current_text != "":
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, split_nodes_image, split_nodes_link
 from src.textnode import TextNode, TextType
 
 
@@ -19,6 +19,23 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode("This is `broken markdown", TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+
+def test_split_images(self):
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
+    self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+        ],
+        new_nodes,
+    )
 
 
 if __name__ == "__main__":
